71672f400b6a06172efbe8e8727157e180c1b51e
Deep <deep.patel@smartsensesolutions.com>
2023-05-06 16:52:47 +0530

    [CXPM-123] add new config flow

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
 import './App.css'
-import { Button } from 'kit'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <header>This is header</header>
+  return <header>This is headersssss</header>
 }
 
 export default App
